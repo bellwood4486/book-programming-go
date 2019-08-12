70fd10736ba44d7aa5f815773e51351aacba5dc3
SUZUKI Yoshiharu <bellwood4486@gmail.com>
2019-08-12 21:36:22 +0900

    Add an answer of 3.13

---
+++ ch03/3.13/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+const (
+	B  = 1
+	KB = 1000 * B
+	MB = 1000 * KB
+	GB = 1000 * MB
+	TB = 1000 * GB
+	PB = 1000 * TB
+	EB = 1000 * PB
+	ZB = 1000 * EB
+	YB = 1000 * ZB
+)
+
+func main() {
+	fmt.Println(KB)
+}
